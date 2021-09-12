097feb4eadf57545d9d89b4c21113c5120604b9c
aayushi221 <aayushipandey221@gmail.com>
2021-09-12 20:55:03 +0530

    🔥 done

--- DAY-9THA/routes/users.js
+++ DAY-9THA/routes/users.js
@@ -1,17 +1,25 @@
 var express = require('express');
+const fs = require('fs')
 const path = require('path');
 var router = express.Router();
 
 /* GET users listing. */
 
 
-router.get('/file/:filename', function (req, res) {
+router.get('/file/:filename(*$)', function (req, res) {
   const filename = req.params.filename
   const fullpath = path.join(__dirname, "../public/images")
-  // res.send(absolutepath);
-  // console.log(filename);
-  res.sendFile(path.join(fullpath, filename));
-  // res.send('respond with a resource');
+  const abspath = path.join(fullpath, filename)
+
+  if (fs.statSync(abspath).isFile()) {                // checks if it is a folder or a file
+    if (fs.existsSync(path.join(fullpath, filename))) //prevents from routes being visible
+      res.sendFile(path.join(fullpath, filename));
+    else
+      res.sendStatus(404)
+    // res.send('respond with a resource');
+  } else {
+    res.sendStatus(404).send('it is a folder')
+  }
 });
 
 router.get('/file', function (req, res) {
